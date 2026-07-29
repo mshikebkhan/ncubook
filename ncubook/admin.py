4497abc229dad713141ec04d8ba53c172971bf91
Shikeb Khan <mshikebkhan@gmail.com>
2026-07-29 13:54:55 +0530

    Updated

--- ncubook/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Announcement
-
-admin.site.register(Announcement)
--- ncubook/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NcubookConfig(AppConfig):
-    name = 'ncubook'
